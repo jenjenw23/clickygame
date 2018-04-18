667ad0880ef17bb05592158974fe3921932a90c5
Woulas <jwoula01@presencehealth.net>
2018-04-18 08:57:23 -0500

    header changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,23 @@
 import React from "react";
 
 const Navbar = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar</a>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <a className="navbar-brand" href="/">Clicky Game</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarNav">
+
     <ul className="navbar-nav">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
+      <li class="">Click an image to begin!</li>  
+      <li>Score: 0 | Top Score: 0</li>
+      {/* <li className="nav-item active">
+        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+      </li> */}
+      {/* <li className="nav-item">
         <a className="nav-link" href="#">Features</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Pricing</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link disabled" href="#">Disabled</a>
-      </li>
+      </li> */}
+
     </ul>
   </div>
 </nav>
